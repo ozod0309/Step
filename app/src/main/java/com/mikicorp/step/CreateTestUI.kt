46b0de9686b8b14e287f4b0136247aa76ae3ace0
ozod0309 <mirozod@gmail.com>
2024-10-23 18:42:40 +0500

    Creating Test AI

--- app/src/main/java/com/mikicorp/step/CreateTestUI.kt
+++ app/src/main/java/com/mikicorp/step/CreateTestUI.kt
@@ -2,19 +2,26 @@ package com.mikicorp.step
 
 import android.content.Context
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.QuestionAnswer
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,12 +35,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class CreateTestUI(val context: Context?, private var docText: String) {
+    private var myTest = arrayListOf<MyTest>()
+    private val docList = arrayListOf<DocToList>()
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun UI(
         onClose: () -> Unit = {}
     ) {
-        val docList = docText.lines()
+        docText.lines().map {str ->
+            docList.add(DocToList(str, false))
+        }
+        DocToTest()
         val scroll = rememberScrollState(0)
         Scaffold(
             topBar = {
@@ -56,7 +69,7 @@ class CreateTestUI(val context: Context?, private var docText: String) {
                     modifier = Modifier
                         .padding(innerPadding)
                 ) {
-//                    Spacer(modifier = Modifier.height(20.dp))
+                    Spacer(modifier = Modifier.height(20.dp))
 //                    ShowQuestion(activeQuestion)
 //                    Spacer(modifier = Modifier.height(20.dp))
 //                    HorizontalDivider()
@@ -98,10 +111,38 @@ class CreateTestUI(val context: Context?, private var docText: String) {
 //                        }
 //                    }
                     HorizontalDivider()
-                    Text(
-                        modifier = Modifier.verticalScroll(scroll),
-                        text = docText
-                    )
+                    LazyColumn {
+                        items(docList.size) {index ->
+                            Row {
+                                Spacer(modifier = Modifier.width(5.dp))
+                                Icon(
+                                    imageVector = Icons.Filled.QuestionAnswer,
+                                    tint = MaterialTheme.colorScheme.primary,
+                                    contentDescription = null
+                                )
+                                Spacer(modifier = Modifier.width(5.dp))
+                                Icon(
+                                    imageVector = Icons.Filled.Star,
+                                    tint = MaterialTheme.colorScheme.primary,
+                                    contentDescription = null
+                                )
+                                Spacer(modifier = Modifier.width(5.dp))
+                                Icon(
+                                    imageVector = if (docList[index].enabled)
+                                        Icons.Filled.Add
+                                    else
+                                        Icons.Filled.Cancel,
+                                    tint = MaterialTheme.colorScheme.primary,
+                                    contentDescription = null
+                                )
+                                Spacer(modifier = Modifier.width(15.dp))
+                                Text(
+                                    text = docList[index].str,
+                                    modifier = Modifier.fillMaxWidth(),
+                                )
+                            }
+                        }
+                    }
                 }
             },
             bottomBar = {
@@ -132,8 +173,40 @@ class CreateTestUI(val context: Context?, private var docText: String) {
         )
     }
 
+    private fun DocToTest() {
+        var i = 0
+        while(i < docList.size) {
+            val str = docList[i].str
+            val question = str
+            val answers = arrayListOf<MyAnswer>()
+//            while(str[0] == 'a') {
+//
+//            }
+            i++
+        }
+    }
+
 
 }
+
+data class DocToList(
+    val str: String,
+    val isQuestion: Boolean = false,
+    val isAnswer: Boolean = false,
+    val enabled: Boolean = false
+)
+
+data class MyTest(
+    val question: String,
+    val image: String = "",
+    val answers: ArrayList<MyAnswer>,
+)
+
+data class MyAnswer(
+    val answer: String,
+    val image: String = "",
+    val correct: Boolean = false
+)
 
 @Preview
 @Composable
